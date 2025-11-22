cf5e9f85a4abc59d3e1a34908265cf4372ba19c4
don <donalduck254@gmail.com>
2025-11-22 09:22:11 +0300

    assignment

--- gardenManagement.js
+++ gardenManagement.js
@@ -1,3 +1,29 @@
 const temperature = 90;
 const timeOfDay = "morning";
 let soilMoisture = 30;
+
+//Temperature test 
+
+
+if (temperature > 80){
+    console.log("watering on")
+}
+else{
+    console.group("watering off")
+}
+
+// light test
+if(timeOfDay === "evening"||timeOfDay === "night"){
+    console.log("Lights on")
+}
+else{
+    console.log("Lights off")
+}
+
+// soil_moisture test 
+
+while (soilMoisture < 40){
+  console.log(soilMoisture)
+  soilMoisture+=5;
+}
+console.log(soilMoisture)
